4899ae2529690ad246185afab7e609d1b884b295
孙茂森 <maosen.sun@1mifudao.com>
2018-03-16 15:09:36 +0800

    Initial the page of project

---
+++ static/js/8.fd1eb436c05217cc9ef5.js
@@ -0,0 +1,2 @@
+webpackJsonp([8],{Si9P:function(e,t){},V52n:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"blank"},[t("router-view")],1)},staticRenderFns:[]};var r=n("VU/8")({},a,!1,function(e){n("Si9P")},"data-v-60daa779",null);t.default=r.exports}});
+//# sourceMappingURL=8.fd1eb436c05217cc9ef5.js.map
